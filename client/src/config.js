145ade1c5c37d6d676748a2173eae1e2ce375ad9
revanth-eranki <159441191+revanth-eranki@users.noreply.github.com>
2024-12-26 14:47:57 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 const config = {
     REACT_APP_CLOUDINARY_CLOUD_NAME: "df8sueqaa",
-    REACT_APP_BACKEND_URL: "https://chat-app-rust-sigma-71.vercel.app/",
+    REACT_APP_BACKEND_URL: "https://localhost:8080",
   };
   
 export default config;
