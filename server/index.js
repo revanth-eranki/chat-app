7ccf38580f63ec908e908e9c28e644934be25bae
Eranki Revanth <revantheranki2005@gmail.com>
2025-01-05 19:23:11 +0530

    updted server

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const connectDB = require('./config/connectDB');
 const router = require('./routes/index');
 const cookiesParser = require('cookie-parser');
 const { app, server } = require('./socket/index');
-const path = require('path');
+// const path = require('path');
 
 // CORS Options
 const corsOptions = {
@@ -20,15 +20,15 @@ app.use(express.json());
 app.use(cookiesParser());
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build')));
+// app.use(express.static(path.join(__dirname, '../client/build')));
 
 // API endpoints
 app.use('/api', router);
 
 // Serve React app for any non-API routes
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+// });
 
 // Database connection and server startup
 const PORT = process.env.PORT || 8080;
